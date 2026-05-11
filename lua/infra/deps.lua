08b9a6374808f4d349f307309cd60c7c3868cdf1
andev0x <anvndev@gmail.com>
2026-05-11 12:18:04 +0700

    perf: optimize configuration

--- lua/infra/deps.lua
+++ lua/infra/deps.lua
@@ -1,60 +1,93 @@
 local M = {}
 
-function M.setup()
-	local add = function(spec)
-		local s = type(spec) == "string" and spec or spec.source
-		if not (s:find("http") or s:find("git@")) then
-			s = "https://github.com/" .. s
-		end
+-- Normalize plugin specifications for vim.pack
+local function normalize(spec)
+	local source = type(spec) == "string" and spec or spec.source
 
-		local final_spec = type(spec) == "table" and vim.deepcopy(spec) or { src = s }
-		final_spec.src = s
-		final_spec.source = nil
-
-		vim.pack.add({ final_spec })
+	-- Expand GitHub shorthand into full URL
+	if not (source:find("http", 1, true) or source:find("git@", 1, true)) then
+		source = "https://github.com/" .. source
 	end
 
-	-- Use vim.schedule to defer plugin loading and setup,
-	-- keeping the initial startup time below the 20ms target.
+	local final = type(spec) == "table" and vim.tbl_extend("force", {}, spec) or {}
+
+	final.src = source
+	final.source = nil
+
+	return final
+end
+
+-- Batch plugin registration
+local function add(specs)
+	vim.pack.add(vim.tbl_map(normalize, specs))
+end
+
+-- Create helper commands only once
+local function create_commands()
+	if vim.g.pack_commands_created then
+		return
+	end
+
+	vim.g.pack_commands_created = true
+
+	vim.api.nvim_create_user_command("PackUpdate", function()
+		vim.pack.update()
+	end, {
+		desc = "Update plugins",
+	})
+
+	vim.api.nvim_create_user_command("PackClean", function()
+		local inactive = vim.iter(vim.pack.get())
+			:filter(function(plugin)
+				return not plugin.active
+			end)
+			:map(function(plugin)
+				return plugin.spec.name or plugin.spec.src
+			end)
+			:totable()
+
+		if #inactive > 0 then
+			vim.notify("Removing:\n" .. table.concat(inactive, "\n"), vim.log.levels.INFO)
+
+			vim.pack.del(inactive)
+		else
+			vim.notify("No inactive plugins found", vim.log.levels.INFO)
+		end
+	end, {
+		desc = "Remove inactive plugins",
+	})
+end
+
+function M.setup()
+	-- Register user commands immediately
+	create_commands()
+
+	-- Defer plugin loading to keep startup extremely fast.
+	-- This prioritizes perceived responsiveness over eager initialization.
 	vim.schedule(function()
-		add({ source = "neovim/nvim-lspconfig" })
+		add({
+			{ source = "neovim/nvim-lspconfig" },
+
+			{ source = "catppuccin/nvim", name = "catppuccin" },
+
+			{ source = "echasnovski/mini.nvim" },
+
+			{ source = "mfussenegger/nvim-dap" },
+			{ source = "nvim-neotest/nvim-nio" },
+			{ source = "rcarriga/nvim-dap-ui" },
+			{ source = "leoluz/nvim-dap-go" },
+
+			{ source = "Robitx/gp.nvim" },
+
+			{ source = "stevearc/conform.nvim" },
+		})
+
 		require("infra.lsp").setup()
-
-		add({ source = "catppuccin/nvim", name = "catppuccin" })
 		require("plugins.theme").setup()
-
-		add({ source = "echasnovski/mini.nvim" })
 		require("plugins.mini").setup()
-
-		add({ source = "mfussenegger/nvim-dap" })
-		add({ source = "nvim-neotest/nvim-nio" })
-		add({ source = "rcarriga/nvim-dap-ui" })
-		add({ source = "leoluz/nvim-dap-go" })
 		require("plugins.dap").setup()
-
-		add({ source = "Robitx/gp.nvim" })
 		require("plugins.ai").setup()
-
-		add({ source = "stevearc/conform.nvim" })
 		require("plugins.format").setup()
-
-		-- Register helper commands for plugin management
-		vim.api.nvim_create_user_command("PackUpdate", function()
-			vim.pack.update()
-		end, { desc = "Update plugins" })
-
-		vim.api.nvim_create_user_command("PackClean", function()
-			local plugins = vim.iter(vim.pack.get())
-				:filter(function(x) return not x.active end)
-				:map(function(x) return x.spec.name end)
-				:totable()
-			if #plugins > 0 then
-				print("Removing: " .. table.concat(plugins, ", "))
-				vim.pack.del(plugins)
-			else
-				print("No inactive plugins to remove")
-			end
-		end, { desc = "Remove inactive plugins" })
 	end)
 end
 
