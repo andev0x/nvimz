374052e85b9f1a4d1647010c940c50f33d6b9f59
andev0x <anvndev@gmail.com>
2026-05-14 07:52:33 +0700

    chore: count from 1 to n numbers

--- init.lua
+++ init.lua
@@ -35,7 +35,3 @@ require("infra.deps").setup()
 
 -- 8. Track and warn if total actual startup time exceeds the 20ms target
 require("core.startup").track(_G.nvimz_start_time)
-
--- 9.
-vim.opt.number = true
--- vim.opt.relativenumber = true
