b1e5b657ca8b32fd1601081266b31c8d2bcd22a3
andev0x <anvndev@gmail.com>
2026-05-18 22:47:12 +0700

    fix: deprecated highlight

--- lua/core/autocmds.lua
+++ lua/core/autocmds.lua
@@ -4,7 +4,7 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 	group = group,
 	desc = "Highlight yanked text",
 	callback = function()
-		vim.highlight.on_yank({ timeout = 120 })
+		vim.hl.on_yank({ timeout = 120 })
 	end,
 })
 
